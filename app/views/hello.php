be80a38153a553de2fd1ce9201074d5c3dc62db1
omfgitsjack <jackinyiu@gmail.com>
2015-01-10 01:24:15 -0500

    - Added fragment meta tag
    - fixed toast display

--- app/views/hello.php
+++ app/views/hello.php
@@ -4,6 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta charset="utf-8">
 	<meta name="Description" content="Easily browse UTSC's Drop-in Schedule and find people to play with">
+	<meta name="fragment" content="!"> <!-- Prerender -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" href="assets/brand/favicon.ico">
