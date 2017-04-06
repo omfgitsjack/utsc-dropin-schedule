835ebcb313c784b978b874da3444d262209995df
Jack Yiu <jackinyiu@gmail.com>
2017-04-06 00:42:33 -0400

    Update DomScraper.php

--- app/Acme/ScheduleCrawler/Strategies/DomScraper.php
+++ app/Acme/ScheduleCrawler/Strategies/DomScraper.php
@@ -148,6 +148,7 @@ class DomScraper implements IDomScraper
 		// Detect if there are multiple activities in one session
 		$sessions = new Collection();
 		if (count($fields) > 0) { // There may not be drop-in sessions!
+			echo $fields;
 			$activities = $this->parseActivities($fields[0], $fields[4]);
 			
 			foreach ($activities as $activity)
